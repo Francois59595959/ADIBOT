966c4f9171b2ee3e544319c849eb82f25478879e
Francois59595959 <39197225+Francois59595959@users.noreply.github.com>
2020-09-26 21:34:15 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -47,4 +47,4 @@ bot.on('message', message => {
     }
   })
 
-bot.login ('NzEyMzU3NjkzNzcxMjE5MDU1.XsQYzQ.gtKINNK2_J-u9PrraxypUd8je0Y')
+bot.login ('NzEyMzU3NjkzNzcxMjE5MDU1.XsQYzQ.SBFf8SDJqcy4YjaVMzYhJyfGQ4I')
